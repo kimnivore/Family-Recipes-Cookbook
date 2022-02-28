172243bff57cb50215cf25d657d96159f373bc51
Kim Nguyen <kimnivore@gmail.com>
2022-02-28 12:57:10 -0800

    built out server

--- api/server.js
+++ api/server.js
@@ -1,28 +1,27 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
-const db = require('./data/db-config')
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
-function getAllUsers() { return db('users') }
+const authRouter = require('./auth/auth-router');
+const recipesRouter = require('./recipes/recipes-router');
 
-async function insertUser(user) {
-  // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-  // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-  const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
-  return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-}
+const { restricted } = require('./auth/auth-middleware');
 
-const server = express()
-server.use(express.json())
-server.use(helmet())
-server.use(cors())
+const server = express();
 
-server.get('/api/users', async (req, res) => {
-  res.json(await getAllUsers())
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+//server.use('/api/auth', authRouter);
+//server.use('/api/recipes', restricted, recipesRouter);
+
+server.use((err, req, res, next) => { //eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack
+  })
 })
 
-server.post('/api/users', async (req, res) => {
-  res.status(201).json(await insertUser(req.body))
-})
 
-module.exports = server
+module.exports = server;
